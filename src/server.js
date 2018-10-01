76653a5a27286958202da4c8117cf11275de871d
omar.melendrez <omar.melendrez@globant.com>
2018-10-01 14:55:58 -0300

    Fix const name

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
-cont express = require('express');
-cont path = require('path');
-cont serveStatic = require('serve-static');
+const express = require('express');
+const path = require('path');
+const serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
-cont port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
