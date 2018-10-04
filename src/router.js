d1d3f7b5c308e1ffab458fae9a5ebfdd4c096d1f
omar.melendrez <omar.melendrez@globant.com>
2018-10-04 16:34:34 -0300

    Implement form validation and reset password form

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home.vue'
 import Login from '@/views/Login.vue'
+import ForgotPassword from '@/views/ForgotPassword.vue'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/forgot-password',
+      name: 'ForgotPassword',
+      component: ForgotPassword
     }
   ]
 })
