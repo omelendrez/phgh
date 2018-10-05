ccda80bad18e32064ab23578c6348c7542c38add
omar.melendrez <omar.melendrez@globant.com>
2018-10-05 14:26:40 -0300

    add design page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 // https://medium.com/@oleg.agapov/basic-single-page-application-using-vue-js-and-firebase-part-1-9e4c0c11a228
-
+// http://rws-fulfillmentengine-portal.azurewebsites.net/ActivityReport
 import Vue from 'vue'
 import Router from 'vue-router'
 
