21cfbb3d1f4c3d485a1cbacb9047eeefa6cd3c5f
omar.melendrez <omar.melendrez@globant.com>
2018-10-05 12:59:11 -0300

    add firebase

--- src/main.js
+++ src/main.js
@@ -3,11 +3,21 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 import Vuetify from 'vuetify'
+import firebase from 'firebase'
 import './registerServiceWorker'
 
 Vue.use(Vuetify)
 
 Vue.config.productionTip = false
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBUnhsAQR0501sD_qllcxjv0jvz8w9kBIU",
+  authDomain: "phgh-c22c4.firebaseapp.com",
+  databaseURL: "https://phgh-c22c4.firebaseio.com",
+  projectId: "phgh-c22c4",
+  storageBucket: "phgh-c22c4.appspot.com",
+  messagingSenderId: "95453109205"
+})
 
 import 'vuetify/dist/vuetify.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
