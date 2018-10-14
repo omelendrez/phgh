1d1897acd3fb7ea2ff02e872e4478ebe3ee2ba3c
omelendrez <omar.melendrez@gmail.com>
2018-10-13 21:51:37 -0300

    Min password size will be 6 chars

--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -36,7 +36,7 @@ export default {
     showPassword: false,
     rules: {
       required: value => !!value || 'Required.',
-      min: v => v.length >= 8 || 'Min 8 characters'
+      min: v => v.length >= 6 || 'Min 6 characters'
     }
   }),
   watch: {
