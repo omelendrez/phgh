d519de747eac00e4b8a159f2a175a56e9341c823
omelendrez <omar.melendrez@gmail.com>
2018-10-15 00:47:53 -0300

    Decouple config options and dashboard

---
+++ src/components/Dash.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container>
+    <v-layout justify-center>
+      <v-flex xs12 sm6>
+        <v-card flat>
+          <v-container fluid grid-list-md>
+            <v-layout row wrap>
+              <v-flex v-for="card in cardsList" v-bind="{ [`xl${card.flex}`]: true }" :key="card.title">
+                <v-card :color="card.color" class="white--text">
+                  <v-card-title>
+                    {{card.title}}
+                  </v-card-title>
+                  <v-card-text class="text-xs-center">
+                    <h2>{{card.value}}</h2>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-btn flat color="#f9aa33">See details</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Dash',
+  props: ['items'],
+  data: () => ({
+    cardsList: []
+  }),
+  watch: {
+    items () {
+      this.cardsList = this.items
+    }
+  },
+  created () {
+    this.cardsList = this.items
+  }
+}
+</script>
--- src/views/settings/config/GH.vue
+++ src/views/settings/config/GH.vue
@@ -5,6 +5,7 @@
 <script>
 import store from '@/store/index'
 import List from '@/components/List'
+import config from '@/config'
 
 export default {
   name: 'GH',
@@ -13,48 +14,11 @@ export default {
   },
   store,
   data: () => ({
-    items: [
-      {
-        title: 'Days PH to payback',
-        subtitle: 'Number of days to PH back after GH.',
-        type: 'text',
-        suffix: 'Days',
-        fieldType: 'number',
-        min: 1,
-        value: 4
-      },
-      {
-        title: 'Percent for recommits',
-        subtitle: 'Percentage to recommit after GH',
-        type: 'text',
-        suffix: '%',
-        fieldType: 'number',
-        min: 0,
-        value: 10
-      },
-      {
-        title: 'Bonus percent for recommits',
-        subtitle: 'Percentage of bonus when recommit after bonus',
-        type: 'text',
-        suffix: '%',
-        fieldType: 'number',
-        min: 0,
-        value: 50
-      },
-      {
-        title: 'Release days',
-        subtitle: 'Number of days release shall be made after payment confirmed',
-        type: 'text',
-        suffix: 'days',
-        fieldType: 'number',
-        min: 0,
-        value: 21
-      }
-    ]
-
+    items: []
   }),
   created () {
     store.dispatch('setAppTitle', 'GH Config')
+    this.items = config['gh']
   }
 }
 
