6a7b57280cac62e9d92d3618401c98a8364de492
omelendrez <omar.melendrez@gmail.com>
2018-10-21 20:24:36 -0300

    Fix dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -18,8 +18,8 @@ export default {
       { title: 'Database', value: 'OK', color: '#356859', flex:  2 },
       { title: 'PH Amount', value: 'NGN 1,621,765', color: '#344955', flex: 3 },
       { title: 'GH Amount', value: 'NGN 1,442,089', color: '#344955', flex: 3 },
-      { title: 'Bitcoins for PH', value: 'NGN 432,200', color: '#4a6572', flex: 3 },
-      { title: 'Bitcoins for GH', value: 'NGN 440,523', color: '#4a6572', flex: 3 },
+      { title: 'Bitcoins for PH', value: '432,200', color: '#4a6572', flex: 3 },
+      { title: 'Bitcoins for GH', value: '440,523', color: '#4a6572', flex: 3 },
       { title: 'Transactions', value: '932', color: '#356859', flex: 2 },
       { title: 'Blocked participants', value: '83', color: '#356859', flex: 2 }
     ]
