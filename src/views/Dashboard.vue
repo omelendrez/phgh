f253fbbfd52fe0407d5b3518743a742ebb96933c
omelendrez <omar.melendrez@gmail.com>
2018-10-21 21:23:52 -0300

    Dashboard changes

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -18,8 +18,8 @@ export default {
       { title: 'Database', value: 'OK', color: '#356859', flex:  2 },
       { title: 'PH in Money', value: 'NGN 1,621,765', color: '#344955', flex: 3 },
       { title: 'GH in Money', value: 'NGN 1,442,089', color: '#344955', flex: 3 },
-      { title: 'PH in Bitcoins', value: '432,200', color: '#4a6572', flex: 3 },
-      { title: 'GH in Bitcoins', value: '440,523', color: '#4a6572', flex: 3 },
+      { title: 'PH in Bitcoins', value: '1.432,200', color: '#4a6572', flex: 3 },
+      { title: 'GH in Bitcoins', value: '1,440,523', color: '#4a6572', flex: 3 },
       { title: 'Transactions', value: '932', color: '#356859', flex: 2 },
       { title: 'Blocked participants', value: '83', color: '#356859', flex: 2 }
     ]
