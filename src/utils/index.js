af9e60179ea4024fa340bd4e6da496ad5e04eb43
omar.melendrez <omar.melendrez@globant.com>
2018-10-22 08:33:59 -0300

    Fix frontend

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,7 @@
 const API = 'https://phgh-be.herokuapp.com/v1/' // 'https://phgh-be.herokuapp.com/v1/'
 const defaultPassword = 'FITTOC'
 const rules = {
-  required: v => !!v || 'Required.',
+  required: v => !!v || 'Required',
   min: v => v.length >= 6 || 'Min 6 characters',
   validEmail: v => /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v) || 'E-mail must be valid'
 }
